89fc0546a375ad386c6373b8e0d8183db362125c
stas97 <stas97@hotmail.com>
2021-06-10 13:36:30 +0200

    cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Project for the course',
+    author='The BaddesT MLOppers Alive',
+    license='',
+)
