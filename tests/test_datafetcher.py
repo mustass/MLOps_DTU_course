cbbf00c793ef9d2a6cc57d31abe6d09852aea38a
mapl <placenti.marco@gmail.com>
2021-06-14 22:00:25 +0200

    attempting to fix test running

--- tests/test_datafetcher.py
+++ tests/test_datafetcher.py
@@ -4,7 +4,7 @@ from src.data.fetch_dataset import ensemble, parse_datasets, download_if_not_exi
 
 @pytest.mark.parametrize(
     "dataset_name", ["CD and Vinyl", "Video Games", "video_games", "baby"])
-def test(dataset_name):
+def test_dataset(dataset_name):
     with pytest.raises(ValueError,
                        match=r"Requested dataset does not exists on server."):
         download_dataset(dataset_name)
