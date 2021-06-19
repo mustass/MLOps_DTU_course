99d2cef912d42ea2ddb4df7b58e90de603e3dd77
mapl <placenti.marco@gmail.com>
2021-06-19 15:18:35 +0200

    updated entry script for deployment

--- src/web-service/entry_script.py
+++ src/web-service/entry_script.py
@@ -5,16 +5,24 @@ from azureml.core import Workspace, Run
 from azureml.core.model import Model
 import yaml
 import os
+from src.models.model import BERT_model
 
 # Called when the service is loaded
 def init():
     global model
-    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), '/models/monki-see-monki-sleep')
+    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 
+                            '/models/monki-see-monki-sleep')
+    with open(os.path.join(os.getenv('AZUREML_MODEL_DIR'), 
+                                '/config/config.yml')) as f:
+        config = yaml.safe_load(f)
 
-    #global model
-    #model = Model(ws, flags['experiment_name'])
-    #model = BERT_model.load_from_checkpoint(model_path)
-    #model = joblib.load(model_path)
+    full = config['training']['full']
+    lr = config['training']['lr']
+    datasets = config['data']['used_datasets']
+    classes = sum([1 for k, v in datasets.items() if v==1])
+
+    model = BERT_model(full, n_class=classes, lr=lr)
+    model = model.load_from_checkpoint(model_path)
 
 # Called when a request is received
 def run(raw_data):
