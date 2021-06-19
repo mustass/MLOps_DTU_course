82ed19b7d650c9218b002ad1d6fa74de3c2a671d
AsgerMunch <81990442+AsgerMunch@users.noreply.github.com>
2021-06-19 18:01:18 +0200

    Add files via upload

---
+++ tests/test_model.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 19 10:51:49 2021
+
+@author: asgermunch
+"""
+
+import yaml
+from src.models.model import BERT_model
+from src.data.lightning_data_module import MyDataModule
+from src.data.fetch_dataset import parse_datasets
+from transformers import AutoModel
+
+def test_model(config_path='config/config.yml'):
+    with open(config_path) as f:
+            config = yaml.safe_load(f)
+    data = MyDataModule(config)
+    datasets = parse_datasets(config)
+    
+    # Initialise model
+    lr = config['training']['lr']
+    bert = AutoModel.from_pretrained('bert-base-uncased')
+    model = BERT_model(bert, n_class=len(datasets), lr=lr)
+    
+    # Some test of the model
+    data.prepare_data()
+    data.setup()
+    data = next(iter(data.train_dataloader()))
+    seq = data[0]
+    mask = data[1]
+    # labels=data[2]
+    outputs = model(seq,mask)
+    assert outputs.shape == (config['training']['batch_size'],len(datasets)), 'The output shape is not correct, expected shape {} but got shape {}'.format([config['training']['batch_size'],len(datasets)], [shap for shap in outputs.shape])
