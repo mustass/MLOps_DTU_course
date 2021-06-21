ff7a63c5b5801590f0559ef5462bbe45e0e14624
mapl <placenti.marco@gmail.com>
2021-06-21 13:28:21 +0200

    works

--- src/webservice/entry_script.py
+++ src/webservice/entry_script.py
@@ -8,22 +8,17 @@ def init():
     with open('src/webservice/config.yml') as f:
         config = yaml.safe_load(f)
 
-    global cwd
-    cwd = os.getcwd()
-    global cwdlist
-    cwdlist = os.listdir(os.getcwd())
     name = config['experiment_name']
     full = config['training']['full']
     lr = config['training']['lr']
     datasets = config['data']['used_datasets']
-    #classes = sum([1 for k, v in datasets.items() if v==1])
+    classes = sum([1 for k, v in datasets.items() if v==1])
 
-    model_path = 'webservice/'+name
-    #print(model_path)
-    #model = BERT_model(full, n_class=classes, lr=lr)
-    #model = model.load_from_checkpoint(model_path)
+    model_path = 'src/webservice/'+name
+    model = BERT_model(full, n_class=classes, lr=lr)
+    model = model.load_from_checkpoint(model_path)
 
 # Called when a request is received
 def run(raw_data):
-    return ([0 for i in raw_data], cwd, cwdlist)
+    return 0
     
