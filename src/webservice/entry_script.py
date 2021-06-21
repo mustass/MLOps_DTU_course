219231ca1979476ae24417fb5a1c17d3a69bbfbc
mapl <placenti.marco@gmail.com>
2021-06-21 10:00:14 +0200

    cloude deployment

--- src/webservice/entry_script.py
+++ src/webservice/entry_script.py
@@ -5,7 +5,7 @@ from azureml.core import Workspace, Run
 from azureml.core.model import Model
 import yaml
 import os
-#from webservice.model import BERT_model
+from src.webservice.model import BERT_model
 
 # Called when the service is loaded
 def init():
@@ -17,8 +17,9 @@ def init():
     full = config['training']['full']
     lr = config['training']['lr']
     datasets = config['data']['used_datasets']
-    #classes = sum([1 for k, v in datasets.items() if v==1])
+    classes = sum([1 for k, v in datasets.items() if v==1])
 
+    global model_path
     model_path = 'webservice/'+name
     #print(model_path)
     #model = BERT_model(full, n_class=classes, lr=lr)
@@ -27,5 +28,6 @@ def init():
 # Called when a request is received
 def run(raw_data):
     print(raw_data)
+    print(model_path)
     return [0 for i in raw_data]
     
